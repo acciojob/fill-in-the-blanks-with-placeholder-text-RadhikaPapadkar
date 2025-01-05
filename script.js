a4c8663f296f0c8db3f415866bf9ff35d5c6837d
RadhikaPapadkar <radhikapapadkar226@gmail.com>
2025-01-05 21:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+   // Create and add the h1 element
+        const h1 = document.createElement('h1');
+        h1.textContent = 'Hello World';
+        document.body.appendChild(h1);
+
+        // Create and add the h2 element
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Introducing the Accio Ipsum';
+        document.body.appendChild(h2);
+
+        // Create and add the paragraph element
+        const p = document.createElement('p');
+        p.textContent = 'Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.';
+        document.body.appendChild(p);
+    </script>
+</body>
+</html>
